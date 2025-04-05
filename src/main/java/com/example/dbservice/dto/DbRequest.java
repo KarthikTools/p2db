5c5c5be861fb808b72a01b41d8e5567f1e1d1831
Karthiks Repo <74430751+KarthikTools@users.noreply.github.com>
2025-04-05 13:11:07 -0400

    Update DbRequest.java
    added getter setter

--- src/main/java/com/example/dbservice/dto/DbRequest.java
+++ src/main/java/com/example/dbservice/dto/DbRequest.java
@@ -1,14 +1,63 @@
 package com.example.dbservice.dto;
 
-import lombok.Data;
-
-@Data
 public class DbRequest {
-    private String host;                   
-    private int port;                      
-    private String user;                   
-    private String password;               
+    private String host;
+    private int port;
+    private String user;
+    private String password;
     private boolean trustServerCertificate;
-    private String database;               
-    private String sql;                    
-} 
+    private String database;
+    private String sql;
+
+    // No-args constructor
+    public DbRequest() {}
+
+    public String getHost() {
+        return host;
+    }
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getUser() {
+        return user;
+    }
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isTrustServerCertificate() {
+        return trustServerCertificate;
+    }
+    public void setTrustServerCertificate(boolean trustServerCertificate) {
+        this.trustServerCertificate = trustServerCertificate;
+    }
+
+    public String getDatabase() {
+        return database;
+    }
+    public void setDatabase(String database) {
+        this.database = database;
+    }
+
+    public String getSql() {
+        return sql;
+    }
+    public void setSql(String sql) {
+        this.sql = sql;
+    }
+}
